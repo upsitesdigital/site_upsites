aa36d2b260291c884b9e98ac660282cf44b69435
brunolobatoalfasoft <115623685+brunolobatoalfasoft@users.noreply.github.com>
2023-03-16 22:50:32 +0000

    atualização

---
+++ html/src/js/modules/slider.js
@@ -0,0 +1,115 @@
+/**
+ * Slider
+ */
+export default function() {
+
+  const $clientSlide = $('.clientSlide');
+  $clientSlide.slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: false,
+    arrows: true,
+    nextArrow: '<button class="slick-next slick-arrow" aria-label="Next" type="button"><svg class="icon"><use xlink:href="assets/img/icons.svg#arrowSlide"></use></svg></button>',
+    prevArrow: '<button class="slick-prev slick-arrow" aria-label="Previous" type="button"><svg class="icon"><use xlink:href="assets/img/icons.svg#arrowSlide"></use></svg></button>',
+    centerMode: true,
+    focusOnSelect: true,
+    adaptiveHeight: true,
+    centerPadding: '0',
+    responsive: [{
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 3,
+      }
+    }, {
+      breakpoint: 481,
+      settings: {
+        slidesToShow: 2,
+      }
+    }]
+  });
+
+  const $boxslide = $('.box-slide');
+  $boxslide.slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    appendDots: $('.slide-grafics .box-dots'),
+    arrows: true,
+    nextArrow: $('.slide-grafics .container-small .next'),
+    prevArrow: $('.slide-grafics .container-small .prev'),
+    centerMode: true,
+    focusOnSelect: true,
+    adaptiveHeight: true,
+    centerPadding: '0'
+  });
+
+  const $recoverypenaltiesslide = $('.recovery-penalties-slide');
+  $recoverypenaltiesslide.slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: true,
+    nextArrow: $('.recovery-penalties .next'),
+    prevArrow: $('.recovery-penalties .prev'),
+    centerMode: true,
+    focusOnSelect: true,
+    adaptiveHeight: true,
+    centerPadding: '0',
+    responsive: [{
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 3,
+        arrows: false,
+      }
+    }, {
+      breakpoint: 481,
+      settings: {
+        slidesToShow: 1,
+        adaptiveHeight: true,
+      }
+    }]
+  });
+
+  const $status = $('.pagingInfo');
+  const $testimonySlide = $('.testimonySlide');
+  const changeSlide = (slider, action) => {
+    $(slider).slick(action);
+  };
+
+  $('#slick-next').on('click', () => {
+    changeSlide($testimonySlide, 'slickNext');
+  });
+
+  $('#slick-prev').on('click', () => {
+    changeSlide($testimonySlide, 'slickPrev');
+  });
+
+  $testimonySlide.on('init reInit afterChange', function(event, slick, currentSlide) {
+    const i = (currentSlide ? currentSlide : 0) + 1;
+    const n = (i <= 9 ? '0' + i : i);
+    const c = (slick.slideCount <= 9 ? '0' + slick.slideCount : slick.slideCount);
+    $status.text(n + ' • ' + c);
+  });
+
+  $testimonySlide.slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    centerMode: false,
+    focusOnSelect: true,
+    adaptiveHeight: true,
+    centerPadding: '0',
+    responsive: [{
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+      }
+    }, {
+      breakpoint: 481,
+      settings: {
+        slidesToShow: 1,
+      }
+    }]
+  });
+}
---
+++ html/src/js/pages/_home.js
@@ -0,0 +1,26 @@
+/**
+ * Home
+ */
+export default function() {
+  $(document).on('click', '.anchor', function() {
+    var $target = $(this).attr('href');
+    var $offset = $($target).position().top;
+    $('body,html').animate({ scrollTop: $offset }, 1000);
+    return false;
+  });
+
+  $(document).on('click', '.accordion > .accordionClick', function() {
+    $(this).closest('.accordion').toggleClass('accordionOpen').find('.accordionContent').slideToggle();
+    return false;
+  });
+
+
+  $(document).on('click', '#open-menu', function() {
+    $('.seach-box').removeClass('act');
+    $('body').toggleClass('menu-open');
+    $(this).toggleClass('opened').attr('aria-expanded', 'true');
+
+    return false;
+  });
+
+}
